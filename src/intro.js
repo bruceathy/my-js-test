037529046f5f1085aad39cbb88a4fd03c71f597d
bruceathy <67832593+bruceathy@users.noreply.github.com>
2025-09-08 23:58:24 -0400

    up

--- src/intro.js
+++ src/intro.js
@@ -1,6 +1,6 @@
 //Lesson: Writing first tests
-export function max(a, b) {
-  if (a > b) return a;
-  else if (b > a) return b;
-  else return a;
-}
+// export function max(a, b) {
+//   if (a > b) return a;
+//   else if (b > a) return b;
+//   else return a;
+// }
